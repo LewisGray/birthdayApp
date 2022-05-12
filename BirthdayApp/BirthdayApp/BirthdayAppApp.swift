54d25665cbd441274871bbcc4a06bc93e6789522
Lewis <lewistgray2000@yahoo.co.uk>
2022-05-12 12:19:50 +0100

    Initial commit

---
+++ BirthdayApp/BirthdayApp/BirthdayAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdayAppApp.swift
+//  BirthdayApp
+//
+//  Created by Lewis Gray on 12/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct BirthdayAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
